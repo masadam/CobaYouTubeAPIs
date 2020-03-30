0aa592e42ca2ada8ca39e68137cf719102e6feaf
maulana adam sahid <masadamsahid@gmail.com>
2020-03-30 14:07:03 +0700

    Add API Trending Youtube
    Menambahakan API YouTube Trending INdonesia

--- index.php
+++ index.php
@@ -12,8 +12,12 @@ function get_CURL($url)
 }
 
 //MENGAMBIL API YOUTUBE
-$YT_API_Identity_result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCdsE9y553KP7Z7MDXzM1sRg&key=AIzaSyCH1fWHEJwA4gGBZKwckOMHtXhOvMBzZto');
-$YT_API_Videos_result = get_CURL('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCH1fWHEJwA4gGBZKwckOMHtXhOvMBzZto&channelId=UCdsE9y553KP7Z7MDXzM1sRg&maxResults=1&order=date&part=snippet');
+$IDKanalYT = 'UCjwGYOoNHTYzgwtih6oNBfQ';
+$KODE_DAERAH = 'ID';
+
+$YT_API_Identity_result = get_CURL("https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=$IDKanalYT+&key=AIzaSyCH1fWHEJwA4gGBZKwckOMHtXhOvMBzZto");
+$YT_API_Videos_result = get_CURL("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCH1fWHEJwA4gGBZKwckOMHtXhOvMBzZto&channelId=$IDKanalYT&maxResults=1&order=date&part=snippet");
+$YT_API_Trending_Videos_results = get_CURL("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=$KODE_DAERAH&key=AIzaSyCH1fWHEJwA4gGBZKwckOMHtXhOvMBzZto&maxResults=5");
 
 //Data Identitas Channel dari YouTube yg dipakai
 $YTChannelPic = $YT_API_Identity_result['items'][0]['snippet']['thumbnails']['medium']['url']; //Gambar Channel
@@ -24,6 +28,11 @@ $YTChannelViews = $YT_API_Identity_result['items'][0]['statistics']['viewCount']
 
 //Data video terakhir yg diupload Channel YT
 $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
+
+//5 Video Trending Indonesia
+$YTVideoTrending1 = $YT_API_Trending_Videos_results['items'][0]['id'];
+$YTVideoTrending2 = $YT_API_Trending_Videos_results['items'][1]['id'];
+$YTVideoTrending2 = $YT_API_Trending_Videos_results['items'][2]['id'];
 
 ?>
 <!doctype html>
@@ -39,29 +48,13 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
     <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
 
-    <title>My Portfolio</title>
+    <title>Kanal Mas Adam dan Trending Youtube Indonesia</title>
   </head>
   <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#home">Sandhika Galih</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#portfolio">Portfolio</a>
-            </li>
-          </ul>
-        </div>
+      <div class="container justify-content-center">
+        <a class="navbar-brand" href="#home">Selamat Datang!</a>
       </div>
     </nav>
 
@@ -70,8 +63,8 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
       <div class="container">
         <div class="text-center">
           <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+          <h1 class="display-4">Mas Adam</h1>
+          <h3 class="lead">Learn | Design | Develop</h3>
         </div>
       </div>
     </div>
@@ -101,7 +94,7 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
       <div class="container">
         <div class="row pt-4 mb-4">
           <div class="col text-center">
-            <h2>Social Media</h2>
+            <h2>Kanal YouTube Google Developer</h2>
           </div>
         </div>
 
@@ -116,7 +109,7 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
                 <p><?= $YTChannelSubscribers; ?> Berlangganan <!--ambil  Jumlah Subscriber Terkini-->
                 <br><?= $YTChannelVideos; ?> Vidio Upload <!--ambil  Jumlah Video Upload Terkini-->
                 <br><?= $YTChannelViews; ?> Tayangan <!--ambil  Jumlah Total Tayangan Terkini-->
-                <div class="g-ytsubscribe" data-channelid="UCdsE9y553KP7Z7MDXzM1sRg" data-layout="default" data-count="default"></div> <!--Tombol SUbscription-->
+                <div class="g-ytsubscribe" data-channelid="<?=$IDKanalYT?>" data-layout="default" data-count="default"></div> <!--Tombol SUbscription-->
                 </p>
               </div>
             </div>
@@ -128,155 +121,31 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
               </div>
             </div>
           </div>
-          <div class="col-md-5">
-            <div class="row">
-              <div class="col-md-4">
-                <img src="img/profile1.png" width="200" alt="" class="rounded-circle img-thumbnail">
-              </div>
-              <div class="col-md-8">
-                <h5>@ugm.yogyakarta</h5>
-                <p>50000 Followers</p>
-              </div>
+        </div>
+      </section>
+      
+      <section class="social" id="about">
+        <div class="container">
+          <div class="row mb-4">
+            <div class="col text-center">
+              <h2>2 Video Terpopuler di YouTube Indonesia</h2>
             </div>
-
-            <div class="row">
-              <div class="col">
-                <div class="overflow-hidden" style="width:100px; height:100px; float:left; overflow: hidden;">
-                  <img src="img/thumbs/1.png" alt="" width="100px">
-                </div>
-                <div class="overflow-hidden" style="width:100px; height:100px; float:left; overflow: hidden;">
-                  <img src="img/thumbs/2.png" alt="" width="100px">
-                </div>
-                <div class="overflow-hidden" style="width:100px; height:100px; float:left; overflow: hidden;">
-                  <img src="img/thumbs/3.png" alt="" width="100px">
+          </div>
+          <div class="row md-8 mt-3 pb-3 justify-content-center" height="200px">
+            <div class="col pl-8 pr-8">
+              <div class="row mt-3 pb-3">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $YTVideoTrending1; ?>?rel=0" allowfullscreen></iframe> <!--ambil  ID Video Terakhir upload-->
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- Portfolio -->
-    <section class="portfolio" id="portfolio">
-      <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Portfolio</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col pl-8 pr-8">
+              <div class="row mt-3 pb-3">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $YTVideoTrending2; ?>?rel=0" allowfullscreen></iframe> <!--ambil  ID Video Terakhir upload-->
+                </div>
               </div>
             </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>   
-        </div>
-
-        <div class="row">
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div> 
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-
-    <!-- Contact -->
-    <section class="contact bg-light" id="contact">
-      <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Contact</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col-lg-4">
-            <div class="card bg-primary text-white mb-4 text-center">
-              <div class="card-body">
-                <h5 class="card-title">Contact Me</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            
-            <ul class="list-group mb-4">
-              <li class="list-group-item"><h3>Location</h3></li>
-              <li class="list-group-item">My Office</li>
-              <li class="list-group-item">Jl. Setiabudhi No. 193, Bandung</li>
-              <li class="list-group-item">West Java, Indonesia</li>
-            </ul>
-          </div>
-
-          <div class="col-lg-6">
-            
-            <form>
-              <div class="form-group">
-                <label for="nama">Nama</label>
-                <input type="text" class="form-control" id="nama">
-              </div>
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" id="email">
-              </div>
-              <div class="form-group">
-                <label for="phone">Phone Number</label>
-                <input type="text" class="form-control" id="phone">
-              </div>
-              <div class="form-group">
-                <label for="message">Message</label>
-                <textarea class="form-control" id="message" rows="3"></textarea>
-              </div>
-              <div class="form-group">
-                <button type="button" class="btn btn-primary">Send Message</button>
-              </div>
-            </form>
-
-          </div>
         </div>
       </div>
     </section>
@@ -292,11 +161,6 @@ $YTLatestVideoID = $YT_API_Videos_result['items'][0]['id']['videoId'];
         </div>
       </div>
     </footer>
-
-
-
-
-
 
 
     <!-- Optional JavaScript -->
